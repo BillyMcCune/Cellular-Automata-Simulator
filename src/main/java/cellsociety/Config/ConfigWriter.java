4411b65208a0eb0f33cf1ea1815ff104f3a44d46
Billy McCune <wrm29@duke.edu>
2025-01-28 12:36:28 -0500

    Refactor ConfigWriter: added the ConfigWriter class to help write data and/or create a new file for a configuration

---
+++ src/main/java/cellsociety/Config/ConfigWriter.java
@@ -0,0 +1,32 @@
+package cellsociety;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ResourceBundle;
+import javafx.application.Application;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+/**
+ * @author Billy McCune
+ * Purpose:
+ * Assumptions:
+ * Dependecies (classes or packages):
+ * How to Use:
+ * Any Other Details:
+ */
+public class ConfigWriter {
+
+
+
+
+
+}
