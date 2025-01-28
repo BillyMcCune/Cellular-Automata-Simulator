2baf4b755e8c906e5d47b56d3a3177314ba5c80a
Jacob You <jay27@duke.edu>
2025-01-28 16:24:05 -0500

    Feature CELL-01: Added switching enum and integer in states

--- src/main/java/cellsociety/model/data/states/LifeState.java
+++ src/main/java/cellsociety/model/data/states/LifeState.java
@@ -1,9 +1,35 @@
 package cellsociety.model.data.states;
 
 /**
- * Represnts the possible states for the Game of Life simulation
+ * Represents the different possible states for Game of Life
  */
 public enum LifeState {
-  DEAD,
-  ALIVE
+  DEAD(0),
+  ALIVE(1);
+
+  private final int value;
+
+  LifeState(int value) {
+    this.value = value;
+  }
+
+  public int getValue() {
+    return value;
+  }
+
+  /**
+   * Retrieves the LifeState corresponding to the given integer.
+   *
+   * @param value The integer value representing the state.
+   * @return The corresponding LifeState.
+   * @throws IllegalArgumentException If no state is found.
+   */
+  public static LifeState fromInt(int value) {
+    for (LifeState state : LifeState.values()) {
+      if (state.getValue() == value) {
+        return state;
+      }
+    }
+    throw new IllegalArgumentException("Invalid LifeState value: " + value);
+  }
 }
--- src/main/java/cellsociety/model/data/states/PercolationState.java
+++ src/main/java/cellsociety/model/data/states/PercolationState.java
@@ -1,10 +1,36 @@
 package cellsociety.model.data.states;
 
 /**
- * Represents the possible states for the Percolation Simulation
+ * Represents the different possible states for the Percolation Simulation
  */
 public enum PercolationState {
-  BLOCKED,
-  OPEN,
-  PERCOLATED
+  BLOCKED(0),
+  OPEN(1),
+  PERCOLATED(2);
+
+  private final int value;
+
+  PercolationState(int value) {
+    this.value = value;
+  }
+
+  public int getValue() {
+    return value;
+  }
+
+  /**
+   * Retrieves the PercolationState corresponding to the given integer.
+   *
+   * @param value The integer value representing the state.
+   * @return The corresponding PercolationState.
+   * @throws IllegalArgumentException If no state is found.
+   */
+  public static PercolationState fromInt(int value) {
+    for (PercolationState state : PercolationState.values()) {
+      if (state.getValue() == value) {
+        return state;
+      }
+    }
+    throw new IllegalArgumentException("Invalid PercolationState value: " + value);
+  }
 }
