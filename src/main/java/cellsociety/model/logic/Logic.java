615a43c70e813a57e6727212dd917ffdd7ba2dc7
Jacob You <jay27@duke.edu>
2025-01-28 14:34:53 -0500

    Feature CELL-01: Added Logic superclass

--- src/main/java/cellsociety/model/logic/Logic.java
+++ src/main/java/cellsociety/model/logic/Logic.java
@@ -1,5 +1,14 @@
 package cellsociety.model.logic;
 
-public class Logic {
+/**
+ * Abstract superclass responsible for managing the logic of a cellular automaton. Subclasses should
+ * implement specific rules.
+ */
+public abstract class Logic {
 
+  /**
+   * Updates the entire game state by one tick. Subclasses should provide an implementation for this
+   * method.
+   */
+  public abstract void update();
 }
