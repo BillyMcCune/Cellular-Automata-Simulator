5622a896a160883f18cba948adab9415e309219d
Jacob You <jay27@duke.edu>
2025-01-28 13:41:30 -0500

    Feature CELL-08: Added primary methods to Cell class

--- src/main/java/cellsociety/model/data/Cell.java
+++ src/main/java/cellsociety/model/data/Cell.java
@@ -2,4 +2,30 @@ package cellsociety.model.data;
 
 public class Cell {
 
+  private int[] coordinates = new int[2];
+  private int currState;
+  private int nextState;
+
+  public Cell(int row, int col, int state) {
+    coordinates[0] = row;
+    coordinates[1] = col;
+    currState = state;
+    nextState = currState;
+  }
+
+  public int[] getCoordinates() {
+    return coordinates;
+  }
+
+  public int getCurrState() {
+    return currState;
+  }
+
+  public void setNextState(int currState) {
+    this.nextState = currState;
+  }
+
+  public void update() {
+    this.currState = this.nextState;
+  }
 }
