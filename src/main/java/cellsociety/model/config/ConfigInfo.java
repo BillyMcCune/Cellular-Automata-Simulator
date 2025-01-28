12ba63b51a8d4dbb74df49368874e6c9c469c27a
Billy McCune <wrm29@duke.edu>
2025-01-28 12:57:50 -0500

    Refactor ConfigInfo Methods: added basic methods for the ConfigInfo class

--- src/main/java/cellsociety/model/config/ConfigInfo.java
+++ src/main/java/cellsociety/model/config/ConfigInfo.java
@@ -24,5 +24,62 @@ import org.xml.sax.SAXException;
  * Any Other Details:
  */
 public class ConfigInfo extends Application {
+public enum SimulationType {}
+
+
+  //Sets the current config info for the simulation
+  public static void setConfig(ArrayList<>){
+
+  }
+
+  //Returns the simulation type
+  public SimulationType getType(){
+
+  }
+
+  // Returns the author name
+  public String getAuthor(){
+
+  }
+
+  //Returns the title
+  public String getTitle(){
+
+  }
+
+  //Returns the description
+  public String getDescription(){
+
+  }
+
+  //Returns the parameters passed in
+  public ArrayList<> getParameters(){
+
+  }
+
+  //Returns the width
+  public int getWidth(){
+
+  }
+
+  //Returns the height
+  public int getHeight(){
+
+  }
+
+  //Returns the speed
+  public getSpeed(){
+
+  }
+
+  //Returns the grid
+  public getGrid(){
+
+  }
+
+  //Updates the grid
+  public updateGrid(ArrayList cells){
+
+  }
 
 }
