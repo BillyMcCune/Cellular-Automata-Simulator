97f742491161bc1c7a7093b6ee70c8dc653248fc
Hsuan-Kai Liao <hl475@duke.edu>
2025-02-01 18:55:23 -0500

    Refactor DESIGN-02: separate the functions from the scene to controller
    - The controller is the bridge that connects the ConfigIO, Model, and View

---
+++ src/main/java/cellsociety/view/controller/SceneController.java
@@ -0,0 +1,202 @@
+package cellsociety.view.controller;
+
+import cellsociety.model.config.ConfigInfo;
+import cellsociety.model.config.ConfigInfo.SimulationType;
+import cellsociety.model.config.ConfigReader;
+import cellsociety.model.data.Grid;
+import cellsociety.model.data.factories.CellFactory;
+import cellsociety.model.data.factories.FireCellFactory;
+import cellsociety.model.data.factories.LifeCellFactory;
+import cellsociety.model.data.factories.PercolationCellFactory;
+import cellsociety.model.data.states.FireState;
+import cellsociety.model.data.states.LifeState;
+import cellsociety.model.data.states.PercolationState;
+import cellsociety.model.logic.FireLogic;
+import cellsociety.model.logic.LifeLogic;
+import cellsociety.model.logic.Logic;
+import cellsociety.model.logic.PercolationLogic;
+import cellsociety.view.scene.SimulationScene;
+import java.util.List;
+
+/**
+ * The SceneController class is responsible for controlling the flow of the application and managing the different scenes.
+ * It communicates with the ConfigIO parts and the main Model Logics.
+ */
+public class SceneController {
+
+  // ConfigIO
+  private ConfigReader configReader;
+  private ConfigInfo configInfo;
+
+  // Scene
+  private SimulationScene simulationScene;
+
+  // Model
+  private Grid<?> grid;
+  private CellFactory<?> cellFactory;
+  private Logic<?> gameLogic;
+
+  // Instance variables
+  private boolean isLoaded;
+  private boolean isPaused;
+
+  /**
+   * Constructor for the SceneController class.
+   * @param scene The simulation scene to control
+   */
+  public SceneController(SimulationScene scene) {
+    // Initialize
+    this.configReader = new ConfigReader();
+    this.simulationScene = scene;
+    this.isPaused = true;
+  }
+
+  /**
+   * Update the simulation scene with the latest data from the model.
+   */
+  public void update() {
+    // TODO: Implement update method for the model
+    if (!isPaused) {
+      for (int i = 0; i < grid.getNumRows(); i++) {
+        for (int j = 0; j < grid.getNumCols(); j++) {
+          simulationScene.setCell(i, j, grid.getCell(i, j).getCurrentState());
+        }
+      }
+
+      gameLogic.update();
+    }
+  }
+
+  // ConfigIO APIs
+
+  /**
+   * Load the configuration file with the given filename.
+   * @param filename The name of the configuration file to load
+   */
+  public void loadConfig(String filename) {
+    configInfo = configReader.readConfig(filename);
+    if (configInfo != null) {
+      isLoaded = true;
+    }
+  }
+
+  /**
+   * Get a list of all the configuration file names.
+   * @return A list of all the configuration file names
+   */
+  public List<String> getAllConfigFileNames() {
+    return configReader.getFileNames();
+  }
+
+  /**
+   * Get the information about the current configuration.
+   * @return A string containing the information about the current configuration
+   */
+  public String getConfigInformation() {
+    if (configInfo == null) {
+      return "";
+    }
+
+    String info = String.format(
+        "Author: %s\nTitle: %s\nType: %s\nDescription: %s",
+        configInfo.getAuthor(),
+        configInfo.getTitle(),
+        // TODO: Uncomment this line after implementing the getType method in ConfigInfo
+        // configInfo.getType(),
+        ' ',
+        configInfo.getDescription()
+    );
+
+    return info;
+  }
+
+  /**
+   * Get the title of the current configuration.
+   * @return The title of the current configuration
+   */
+  public String getConfigTitle() {
+    if (configInfo == null) {
+      return "";
+    }
+
+    return configInfo.getTitle();
+  }
+
+  /**
+   * Get the speed of the current configuration.
+   * @return The speed of the current configuration
+   */
+  public double getConfigSpeed() {
+    if (configInfo == null) {
+      return 0;
+    }
+
+    return configInfo.getSpeed();
+  }
+
+  // Model APIs
+  /**
+   * Reset the model with the current configuration.
+   */
+  public void resetModel() {
+    // BY: Hsuan-Kai Liao
+    // NOTES: Maybe we can redesign a map to take in a SimulationType,
+    //        so that we can get rid of the annoying switch statement.
+    //        This is just a temporary solution.
+
+    if (configInfo == null) {
+      return;
+    }
+
+    // TODO: implement the getType method in ConfigInfo
+    // SimulationType type = configInfo.getType();
+    SimulationType type = SimulationType.GAMEOFLIFE;
+
+    switch (type) {
+      case SimulationType.GAMEOFLIFE -> {
+        cellFactory = new LifeCellFactory();
+        grid = new Grid<>(configInfo.getGrid(), cellFactory);
+        gameLogic = new LifeLogic((Grid<LifeState>) grid);
+      }
+
+      case SimulationType.PERCOLATION -> {
+        cellFactory = new PercolationCellFactory();
+        grid = new Grid<>(configInfo.getGrid(), cellFactory);
+        gameLogic = new PercolationLogic((Grid<PercolationState>) grid);
+      }
+
+      case SimulationType.SPREADINGOFFIRE -> {
+        cellFactory = new FireCellFactory();
+        grid = new Grid<>(configInfo.getGrid(), cellFactory);
+        gameLogic = new FireLogic((Grid<FireState>) grid);
+      }
+
+      // TODO: Add more cases for other simulation types
+    }
+
+    // Set the grid to the scene
+    simulationScene.setGrid(grid.getNumRows(), grid.getNumCols());
+    for (int i = 0; i < grid.getNumRows(); i++) {
+      for (int j = 0; j < grid.getNumCols(); j++) {
+        simulationScene.setCell(i, j, grid.getCell(i, j).getCurrentState());
+      }
+    }
+  }
+
+  // Controller APIs
+  /**
+   * Start or pause the simulation.
+   * @param isPaused True if the simulation should be paused, false otherwise
+   */
+  public void setStartPause(boolean isPaused) {
+    this.isPaused = isPaused;
+  }
+
+  /**
+   * Check if the simulation is currently paused.
+   * @return True if the simulation is paused, false otherwise
+   */
+  public boolean isLoaded() {
+    return isLoaded;
+  }
+}
