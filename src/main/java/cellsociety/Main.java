21ec1d44886fd7ce6bcbe4fe15e69e1bc0fc71d6
Hsuan-Kai Liao <hl475@duke.edu>
2025-02-01 14:16:24 -0500

    Refactor DESIGN-08: clear Rebundent code in Main

--- src/main/java/cellsociety/Main.java
+++ src/main/java/cellsociety/Main.java
@@ -1,42 +1,16 @@
 package cellsociety;
 
-import cellsociety.model.config.ConfigInfo;
-import cellsociety.model.config.ConfigInfo.SimulationType;
-import cellsociety.model.config.ConfigReader;
 import cellsociety.view.scene.SimulationScene;
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.ResourceBundle;
 import javafx.animation.Timeline;
 import javafx.application.Application;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Alert.AlertType;
-import javafx.stage.FileChooser;
 import javafx.stage.Stage;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
 
 /**
- * Feel free to completely change this code or delete it entirely.
+ * The entry point of the program, which sets up the main scene and game loop.
  */
 public class Main extends Application {
 
   public static final int FRAMES_PER_SECOND = 60;
-
-//  @Override
-//  public void start(Stage secondaryStage) {
-//    ConfigReader configReader = new ConfigReader();
-//    List<String> fileNames = configReader.getFileNames();
-//    ConfigInfo myInfo = configReader.readConfig(fileNames.getFirst());
-//    System.out.println(myInfo.getAuthor());
-//  }
-
 
   @Override
   public void start(Stage primaryStage) {
@@ -59,9 +33,7 @@ public class Main extends Application {
 
   /**
    * Start the program, give complete control to JavaFX.
-   * <p>
-   * Default version of main() is actually included within JavaFX, so this is not technically
-   * necessary!
+   * @param args command line arguments
    */
   public static void main(String[] args) {
     launch(args);
