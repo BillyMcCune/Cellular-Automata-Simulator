221041404827e76e103b3e3301b89241b68bd612
Jacob You <jay27@duke.edu>
2025-02-02 14:38:58 -0500

    Refactor DATA: Changed package naming conventions to avoid name conflicts

--- src/test/java/configtests/ConfigInfoTest.java
+++ src/test/java/configtests/ConfigInfoTest.java
@@ -1,11 +1,9 @@
-package ConfigTests;
+package configtests;
 
 import cellsociety.model.config.ConfigInfo;
 import cellsociety.model.config.ConfigReader;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 
