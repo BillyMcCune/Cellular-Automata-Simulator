4ca8398844db31978840372c36a8ec8659a2a2cd
Jacob You <jay27@duke.edu>
2025-02-13 17:00:14 -0500

    Feature CELL-26F: Added base classes for SugarScape simulation

--- src/main/java/cellsociety/model/data/states/SugarState.java
+++ src/main/java/cellsociety/model/data/states/SugarState.java
@@ -1,4 +1,17 @@
 package cellsociety.model.data.states;
 
-public class SugarState {
+public enum SugarState implements State {
+  EMPTY(0),
+  AGENT(1);
+
+  private final int value;
+
+  SugarState(int value) {
+    this.value = value;
+  }
+
+  @Override
+  public int getValue() {
+    return value;
+  }
 }
