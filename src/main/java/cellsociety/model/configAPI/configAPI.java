43b11cc07944a28b128b3a42b4c8bbbb4e16e3f7
Billy McCune <wrm29@duke.edu>
2025-02-28 23:13:20 -0500

    Feature IssueDESIGN-18 - APIs: created second api for configuration as specified - need to implement and have api's work togethor

---
+++ src/main/java/cellsociety/model/configAPI/configAPI.java
@@ -0,0 +1,150 @@
+package cellsociety.model.configAPI;
+
+import cellsociety.model.config.ConfigInfo;
+import cellsociety.model.config.ConfigReader;
+import cellsociety.model.config.ConfigWriter;
+import cellsociety.model.config.ParameterRecord;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+import org.xml.sax.SAXException;
+
+public class configAPI {
+
+  private ConfigReader configReader;
+  private ConfigWriter configWriter;
+  private ConfigInfo configInfo;
+  private ParameterRecord parameterRecord;
+  private boolean isLoaded;
+  /**
+   * Retrieves a list of available configuration file names.
+   *
+   * @return a List of configuration file names.
+   */
+  public List<String> getFileNames() {
+    ConfigReader configReader = new ConfigReader();
+    return configReader.getFileNames();
+  }
+
+  /**
+   * Loads a simulation configuration from the specified file.
+   *
+   * @param fileName the name of the configuration file to load
+   * @throws ParserConfigurationException if a parser configuration error occurs
+   * @throws IOException                  if an I/O error occurs
+   * @throws SAXException                 if a SAX parsing error occurs - check configReader for
+   *                                      more information regarding load simulation errors
+   */
+  public void loadSimulation(String fileName)
+      throws ParserConfigurationException, IOException, SAXException {
+    try {
+      configInfo = configReader.readConfig(fileName);
+      if (configInfo != null) {
+        isLoaded = true;
+        parameterRecord = configInfo.myParameters();
+      }
+    } catch (ParserConfigurationException e) {
+      throw new ParserConfigurationException(e.getMessage());
+    } catch (SAXException e) {
+      throw new SAXException(e.getMessage());
+    } catch (IOException e) {
+      throw new IOException(e.getMessage());
+    }
+  }
+
+  /**
+   * Saves the current simulation configuration to the specified file path
+   *
+   * @param FilePath the file path where the configuration should be saved
+   * @return the file name of the simulation that was saved
+   * @throws ParserConfigurationException if a parser configuration error occurs
+   * @throws IOException                  if an I/O error occurs
+   * @throws TransformerException         if an error occurs during transformation
+   */
+  public String saveSimulation(String FilePath)
+      throws ParserConfigurationException, IOException, TransformerException {
+    ConfigWriter configWriter = new ConfigWriter();
+    configWriter.saveCurrentConfig(configInfo, FilePath);
+    return configWriter.getLastFileSaved();
+  }
+
+
+  /**
+   * Retrieves the accepted simulation states
+   *
+   * @return a of accepted state integers
+   * @throws NullPointerException if the configuration information is not loaded
+   */
+  public Set<Integer> getAcceptedStates() {
+    try {
+      return configInfo.acceptedStates();
+    } catch (NullPointerException e) {
+      throw new NullPointerException("error-configInfo-NULL");
+    }
+  }
+
+  /**
+   * Retrieves the grid width from the configuration
+   *
+   * @return the grid width
+   * @throws NumberFormatException if the configuration information is not loaded
+   */
+  public int getGridWidth() {
+    try {
+      return configInfo.myGridWidth();
+    } catch (NullPointerException e) {
+      throw new NumberFormatException("error-configInfo-NULL");
+    }
+  }
+
+  /**
+   * Retrieves the grid height from the configuration
+   *
+   * @return the grid height
+   * @throws NumberFormatException if the configuration information is not loaded
+   */
+  public int getGridHeight() {
+    try {
+      return configInfo.myGridHeight();
+    } catch (NullPointerException e) {
+      throw new NumberFormatException("error-configInfo-NULL");
+    }
+  }
+
+  public Map<String, String> getSimulationInformation() {
+    if (configInfo == null) {
+      return new HashMap<>();
+    }
+    HashMap<String, String> simulationDetails = new HashMap<>();
+    simulationDetails.put("author", configInfo.myAuthor());
+    simulationDetails.put("title", configInfo.myTitle());
+    simulationDetails.put("type", configInfo.myType().toString());
+    simulationDetails.put("description", configInfo.myDescription());
+
+    return simulationDetails;
+  }
+
+
+
+  public Map<String, String> getSimulationStyles() {
+    return new HashMap<>();
+  }
+
+  public void setSimulationStyles(Map<String, String> simulationStyles) {
+
+  }
+
+
+  public double getConfigSpeed() {
+    try {
+      return configInfo.myTickSpeed();
+    } catch (NullPointerException e) {
+      throw new NumberFormatException("error-configInfo-NULL");
+    }
+  }
+
+}
--- src/main/java/cellsociety/model/modelAPI/ModelAPI.java
+++ src/main/java/cellsociety/model/modelAPI/ModelAPI.java
@@ -1,4 +1,4 @@
-package cellsociety.model.API;
+package cellsociety.model.modelAPI;
 
 import cellsociety.model.config.ConfigInfo;
 import cellsociety.model.config.ConfigInfo.SimulationType;
@@ -25,7 +25,7 @@ import org.xml.sax.SAXException;
  * @author Billy McCune
  */
 //TODO figure out config not loaded error code stuff
-public class CellModelApi {
+public class ModelAPI {
 
   private static final String LOGIC_PACKAGE = "cellsociety.model.logic";
   private static final String STATE_PACKAGE = "cellsociety.model.data.states";
@@ -44,42 +44,6 @@ public class CellModelApi {
   // Instance variables
   private boolean isLoaded;
 
-
-  /**
-   * Retrieves a list of available configuration file names.
-   *
-   * @return a List of configuration file names.
-   */
-  public List<String> getFileNames() {
-    ConfigReader configReader = new ConfigReader();
-    return configReader.getFileNames();
-  }
-
-  /**
-   * Loads a simulation configuration from the specified file.
-   *
-   * @param fileName the name of the configuration file to load
-   * @throws ParserConfigurationException if a parser configuration error occurs
-   * @throws IOException                  if an I/O error occurs
-   * @throws SAXException                 if a SAX parsing error occurs - check configReader for
-   *                                      more information regarding load simulation errors
-   */
-  public void loadSimulation(String fileName)
-      throws ParserConfigurationException, IOException, SAXException {
-    try {
-      configInfo = configReader.readConfig(fileName);
-      if (configInfo != null) {
-        isLoaded = true;
-        parameterRecord = configInfo.myParameters();
-      }
-    } catch (ParserConfigurationException e) {
-      throw new ParserConfigurationException(e.getMessage());
-    } catch (SAXException e) {
-      throw new SAXException(e.getMessage());
-    } catch (IOException e) {
-      throw new IOException(e.getMessage());
-    }
-  }
 
   /**
    * Updates the simulation by invoking the game logic update method.
@@ -102,23 +66,6 @@ public class CellModelApi {
         gameLogic.update();
       }
     }
-  }
-
-
-  /**
-   * Saves the current simulation configuration to the specified file path
-   *
-   * @param FilePath the file path where the configuration should be saved
-   * @return the file name of the simulation that was saved
-   * @throws ParserConfigurationException if a parser configuration error occurs
-   * @throws IOException                  if an I/O error occurs
-   * @throws TransformerException         if an error occurs during transformation
-   */
-  public String saveSimulation(String FilePath)
-      throws ParserConfigurationException, IOException, TransformerException {
-    ConfigWriter configWriter = new ConfigWriter();
-    configWriter.saveCurrentConfig(configInfo, FilePath);
-    return configWriter.getLastFileSaved();
   }
 
 
@@ -149,43 +96,6 @@ public class CellModelApi {
       throw new ClassNotFoundException("error-resetGrid", e);
     }
   }
-
-  public Map<String, String> getSimulationStyles() {
-    return new HashMap<>();
-  }
-
-  public void setSimulationStyles(Map<String, String> simulationStyles) {
-
-  }
-
-  public Map<String, String> getSimulationInformation() {
-    if (configInfo == null) {
-      return new HashMap<>();
-    }
-    HashMap<String, String> simulationDetails = new HashMap<>();
-    simulationDetails.put("author", configInfo.myAuthor());
-    simulationDetails.put("title", configInfo.myTitle());
-    simulationDetails.put("type", configInfo.myType().toString());
-    simulationDetails.put("description", configInfo.myDescription());
-
-    return simulationDetails;
-  }
-
-
-  /**
-   * Retrieves the accepted simulation states
-   *
-   * @return a of accepted state integers
-   * @throws NullPointerException if the configuration information is not loaded
-   */
-  public Set<Integer> getAcceptedStates() {
-    try {
-      return configInfo.acceptedStates();
-    } catch (NullPointerException e) {
-      throw new NullPointerException("error-configInfo-NULL");
-    }
-  }
-
 
   /**
    * Retrieves the double parameters from the parameter record
@@ -281,43 +191,6 @@ public class CellModelApi {
       return parameterRecord.myStringParameters();
     } catch (NullPointerException e) {
       throw new NullPointerException("error-configInfo-NULL");
-    }
-  }
-
-
-  /**
-   * Retrieves the grid width from the configuration
-   *
-   * @return the grid width
-   * @throws NumberFormatException if the configuration information is not loaded
-   */
-  public int getGridWidth() {
-    try {
-      return configInfo.myGridWidth();
-    } catch (NullPointerException e) {
-      throw new NumberFormatException("error-configInfo-NULL");
-    }
-  }
-
-  /**
-   * Retrieves the grid height from the configuration
-   *
-   * @return the grid height
-   * @throws NumberFormatException if the configuration information is not loaded
-   */
-  public int getGridHeight() {
-    try {
-      return configInfo.myGridHeight();
-    } catch (NullPointerException e) {
-      throw new NumberFormatException("error-configInfo-NULL");
-    }
-  }
-
-  public double getConfigSpeed() {
-    try {
-      return configInfo.myTickSpeed();
-    } catch (NullPointerException e) {
-      throw new NumberFormatException("error-configInfo-NULL");
     }
   }
 
